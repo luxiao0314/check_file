0d9526668a74d506a5df0d7c2a90acd0892d56ca
lux <xiao.lu@aihuishou.com>
2020-09-27 17:14:22 +0800

    update

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -3,47 +3,13 @@
     <br />
 
     <div>
-      <span>上传标准文件: </span>
-      <input
-        type="file"
-        ref="refFile1"
-        @change="fileUpload"
-        multiple="multiple"
-      />
+      <span>上传文件: </span>
+      <input type="file" ref="refFile1" @change="fileUpload" />
+
+      <button @click="exportData">导出</button>
     </div>
-
-    <br />
-
-    <div>
-      <span>上传维修文件: </span>
-      <input
-        type="file"
-        ref="refFile2"
-        @change="mutileUpload"
-        multiple="multiple"
-      />
-      <button @click="check">维修正常文件校验</button>
-    </div>
-
-    <br />
-
-    <div>
-      <span>上传结果文件: </span>
-      <input
-        type="file"
-        ref="refFile3"
-        @change="resultUpload"
-        multiple="multiple"
-      />
-
-      <button @click="resultCheck">结果校验</button>
-    </div>
-
-    <br />
 
     <br /><br />
-
-    <div v-for="(line, index) in checkList" :key="index">{{ line }}</div>
 
     <div v-for="(line, index) in checkResultList" :key="index">{{ line }}</div>
   </div>
@@ -55,34 +21,11 @@ export default {
   data() {
     return {
       standardList: [],
-      checkList: [],
-      errlist: [],
-      resultList: [],
       checkResultList: [],
+      filename: "",
     };
   },
   methods: {
-    //维修文件上传
-    mutileUpload() {
-      this.errlist = [];
-      let files = this.$refs.refFile2.files;
-
-      for (var i = 0; i < files.length; i++) {
-        let file = files.item(i);
-        this.filter(file, (items) => {
-          //维修数据不包含就添加
-          items.forEach((it) => {
-            if (!this.errlist.includes(it)) {
-              this.errlist.push(it);
-            }
-          });
-        });
-      }
-
-      console.log("维修数据:");
-      console.log(this.errlist);
-    },
-
     //标准文件上传
     fileUpload() {
       this.standardList = [];
@@ -90,6 +33,8 @@ export default {
 
       for (var i = 0; i < files.length; i++) {
         let file = files.item(i);
+
+        this.filename = file.name
         this.filter(file, (items) => {
           //标准数据不包含就添加
           items.forEach((it) => {
@@ -104,41 +49,11 @@ export default {
       console.log(this.standardList);
     },
 
-    //检测
-    check() {
-      let minus = this.errlist.filter((x) => !this.standardList.includes(x));
-
+    exportData() {
       let results = new Map();
 
       //获取第一个=号,转map去重
-      minus.forEach((it) => {
-        let index = it.indexOf("=");
-        let key = it.substring(0, index);
-        let value = it.substring(index + 1, it.length);
-        results.set(key, value);
-      });
-
-      this.checkList = [];
-
-      //采用map对key去重
-      // for (let item of results) {
-      //   this.checkList.push(item);
-      // }
-
-      for (let [key, value] of results) {
-        this.checkList.push(key + "=" + value);
-      }
-
-      console.log("检测结果:");
-      console.log(this.checkList.sort());
-    },
-
-    //结果筛选
-    resultCheck() {
-      let results = new Map();
-
-      //获取第一个=号,转map去重
-      this.resultList.forEach((it) => {
+      this.standardList.forEach((it) => {
         let index = it.indexOf("=");
         let key = it.substring(0, index);
         let value = it.substring(index + 1, it.length);
@@ -151,43 +66,22 @@ export default {
       for (let [key, value] of results) {
         this.checkResultList.push(key + "=" + value);
       }
-    },
 
-    //结果文件上传
-    resultUpload() {
-      this.resultList = [];
-      let files = this.$refs.refFile3.files;
+      //定义文件内容，类型必须为Blob 否则createObjectURL会报错
+      let content = new Blob([JSON.stringify(this.checkResultList)]);
 
-      for (var i = 0; i < files.length; i++) {
-        let file = files.item(i);
-        this.resultFilter(file, (items) => {
-          //维修数据不包含就添加
-          items.forEach((it) => {
-            if (!this.resultList.includes(it)) {
-              this.resultList.push(it);
-            }
-          });
-        });
-      }
-
-      console.log("结果筛选:");
-      console.log(this.resultList.sort());
-    },
-
-    resultFilter(selectedFile, callback) {
-      let list = [];
-
-      if (!selectedFile) return;
-
-      var reader = new FileReader();
-
-      reader.readAsText(selectedFile);
-
-      reader.onloadend = (e) => {
-        let line = e.target.result.split("\n");
-
-        callback(new Set(line));
-      };
+      //生成url对象
+      let urlObject = window.URL || window.webkitURL || window;
+      let url = urlObject.createObjectURL(content);
+      //生成<a></a>DOM元素
+      let el = document.createElement("a");
+      //链接赋值
+      el.href = url;
+      el.download = this.filename;
+      //必须点击否则不会下载
+      el.click();
+      //移除链接释放资源
+      urlObject.revokeObjectURL(url);
     },
 
     //过滤文件内容
